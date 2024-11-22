ee791905f477d81e7694eced7e0710600722de2a
EudesAlves <eudes.eds@gmail.com>
2024-11-22 16:22:56 -0300

    Criado Mensagens de erro para Login. Criado Validação e mensagens para Registro de Usuário.

--- app_login/views/view_login.py
+++ app_login/views/view_login.py
@@ -11,28 +11,29 @@ def login(request):
     return render(request, 'login/index.html')
 
 def validate_login(request):
-    
+    message = MessageAlert()
     if request.method == 'POST':
         login_email = request.POST.get('login_email')
         login_password = request.POST.get('login_password')
         if not login_email or not login_password:
-            text = "Campos Email e Senha precisam ser preenchidos"
-            error_message = { 'message', text }
-            message = MessageAlert()
-            # TO DO
-            # Criar Mensagens de erro para Página
-            # variavel enviada no render não é reconhecida no Html 
-            render(request, 'login/index.html', { 'test' : message, 'error_message' : error_message } )
-        
+            error_text = "Campos Email e Senha devem ser preenchidos"
+            message.add(error_text)
+
+            return render(request, 'login/index.html', {'messages' : message.messages})
+            
         login = Login()
         query_set = Login.objects.filter(email=login_email)
         if not query_set:
-            return render(request, 'login/index.html')
+            error_text = "Email não está cadastrado"
+            message.add(error_text)
+            return render(request, 'login/index.html', {'messages' : message.messages})
 
         login = query_set.first()
         enconded_pass = LoginUtil.generate_password_hash(login_password, login.random_hash)
         if login.password != enconded_pass:
-            return render(request, 'login/index.html')
+            error_text = "Senha incorreta para o Email informado"
+            message.add(error_text)
+            return render(request, 'login/index.html', {'messages' : message.messages})
             
 
         user = User()
--- app_login/views/view_user.py
+++ app_login/views/view_user.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from app_login.models import User
 from app_login.models import Login
 from app_login.util import LoginUtil
+from app_login.util import MessageAlert
 from app_login.util.LoginUtil import *
+from app_login.util.MessageAlert import *
 
 def home(request):
     users = {
@@ -12,16 +14,32 @@ def home(request):
     return render(request, 'users/home.html', users)
 
 def register(request):
+    message = MessageAlert()
     if request.method == 'POST':
         user_name = request.POST.get('user_name')
         user_email = request.POST.get('user_email')
         login_password = request.POST.get('login_password')
         login_password_confirm = request.POST.get('login_password_confirm')
         
-        if user_name and user_email and login_password and login_password_confirm:
-            user = User()
-            login = Login()
+        if not user_name or not user_email or not login_password or not login_password_confirm:
+            error_text = "Todos os campos devem ser preenchidos"
+            message.add(error_text)
+            return render(request, 'users/register.html', {'messages' : message.messages})
+        
+        query_set = User.objects.filter(email=user_email)
+        if query_set:
+            error_text = "Email já cadastrado no Sistema"
+            message.add(error_text)
+            return render(request, 'users/register.html', {'messages' : message.messages})
+        
+        if login_password != login_password_confirm:
+            error_text = "Campos Senha e Confirmar Senha devem ser iguais"
+            message.add(error_text)
+            return render(request, 'users/register.html', {'messages' : message.messages})
 
+        if user_name and user_email and login_password and login_password_confirm:
+            login = Login()
+            user = User()
             random_hash = LoginUtil.generate_random_hash()
             login.email = user_email
             login.random_hash = random_hash
@@ -34,13 +52,9 @@ def register(request):
             user.login_id = login_id
             user.save()
 
-    # Get registered users
-    users = {
-        'users': User.objects.all()
-    }
 
     # Return data to page
-    return render(request, 'users/register.html', users)
+    return render(request, 'users/register.html')
 
 def update(request, id):
     if request.method == 'GET':
