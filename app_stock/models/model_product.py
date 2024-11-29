f33fac1a08091716bc30ba9b3ce2cea2a39cbd2d
EudesAlves <eudes.eds@gmail.com>
2024-11-29 12:17:45 -0300

    Removido campos da nota fiscal do model de Produto.

--- app_stock/models/model_product.py
+++ app_stock/models/model_product.py
@@ -5,8 +5,4 @@ class Product(models.Model):
 
     name = models.TextField(max_length=255)
 
-    invoice = models.TextField(max_length=255)
-
-    invoice_date = models.DateField()
-
     creation_date = models.DateTimeField(auto_now_add=True)
