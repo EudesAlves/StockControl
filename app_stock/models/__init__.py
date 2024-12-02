1a7b3976a27d64e1131b2e385b7584bc940ac76d
EudesAlves <eudes.eds@gmail.com>
2024-12-02 14:48:18 -0300

    Criado model History. Criado migration.

--- app_stock/models/__init__.py
+++ app_stock/models/__init__.py
@@ -2,3 +2,4 @@ from app_stock.models import model_product
 from app_stock.models import model_technician
 from app_stock.models import model_supplier
 from app_stock.models import model_stock
+from app_stock.models import model_history
---
+++ app_stock/models/model_history.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class History(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    invoice = models.TextField(max_length=100, blank=True)
+
+    invoice_date = models.DateTimeField(blank=True)
+    
+    stock_id = models.IntegerField()
+
+    product_id = models.IntegerField()
+
+    quantity = models.IntegerField()
+
+    supplier_id = models.IntegerField(blank=True)
+
+    technician_id = models.IntegerField(blank=True)
+
+    classification = models.TextField(max_length=100)
+
+    user_id = models.IntegerField()
+
+    creation_date = models.DateTimeField(auto_now_add=True)
